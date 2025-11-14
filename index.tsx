92da0419c6ac8d7330ad4b21f6643574564e71a0
thanhthai198 <thanhthai.ptit.1998@gmail.com>
2025-11-14 15:27:56 +0700

    fix: lint

--- index.tsx
+++ index.tsx
@@ -1,7 +1,3 @@
-import WheelPicker from './src/WheelPicker'
-import TimePicker from './src/TimePicker'
-import DatePicker from './src/DatePicker'
-import { Picker } from '@react-native-picker/picker'
-import RNDateTimePicker from '@react-native-community/datetimepicker';
+import WheelPicker from './src/WheelPicker.android'
 
-export { WheelPicker, TimePicker, DatePicker, Picker, RNDateTimePicker }
+export { WheelPicker }
